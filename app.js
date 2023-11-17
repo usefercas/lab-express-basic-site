23f9f716b059ffd623fcda6e8f9755674d3b5ea8
usefercas <124436934+usefercas@users.noreply.github.com>
2023-11-17 13:12:27 +0100

    ejercicio resuelto

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express = require('express');
-
-
+const path = require('path');
 const app = express();
-app.use(express.static('public')); // todo preguntar porque no hace el import estatico
+const port = 3000;
 
+// Configurar el uso de archivos estáticos en la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Definir la ruta para la página de inicio
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/views/index.html'));
-
-// Definir la ruta para la página Acerca de
-app.get('/about', (req, res) => res.sendFile(__dirname + '/public/views/about.html'));
-
-// Definir la ruta para la página de Trabajos
-app.get('/works', (req, res) => res.sendFile(__dirname + '/public/views/works.html'));
-
-// Hace que el servidor escuche en el puerto 3000
-app.listen(3000, () => {
-  console.log('Servidor iniciado en el puerto 3000');
+// Ruta de la página de inicio
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
 });
 
+// Ruta de la página Acerca de
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'views', 'about.html'));
+});
+
+// Ruta de la página de Trabajos
+app.get('/works', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'views', 'works.html'));
+});
+
+// Ruta de la página de Galería
+app.get('/galeria', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'views', 'galeria.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
